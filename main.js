e6440274ff5829f8a182d46e68341720d759107c
Tomoyuki Sakurai <y@trombik.org>
2024-10-23 22:23:13 +0700

    fixup

--- main.js
+++ main.js
@@ -288,7 +288,7 @@ async function main() {
             const count = await zipfile.extract(null, dir);
             fs.rm(tempDir,{ recursive: true, force: true }, (err) => {
                 core.error(`Error removing tmp directory: ${err}: action may still succeed though`)
-            }
+            })
             core.endGroup()
         }
     } catch (error) {
